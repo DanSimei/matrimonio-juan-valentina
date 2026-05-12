4b9705e6ddfe49c1bb6e7228a16f8e1112c2282b
simgo <simgosu96@gmail.com>
2026-05-12 18:56:45 -0500

    feat: add Header component and update document title and meta tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
         <header className="header">
             <p className="nos-casamos">Nos Casamos</p>
             <img src={`${baseUrl}assets/images/separador1.png`} className="separator-img" alt="Separador" />
-            <h1 className="names">Juan Esteban</h1>
+            <h1 className="names">Juan</h1>
             <span className="ampersand">&</span>
             <h1 className="names">Valentina</h1>
-            
+
             <svg className="vine-divider" viewBox="0 0 200 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10 20 Q50 10 100 20 Q150 30 190 20" stroke="#2d4a1e" strokeWidth="1" />
                 <path d="M40 15 L45 10 M160 15 L155 10" stroke="#b8963e" strokeWidth="1" />
